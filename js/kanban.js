b8e2ef7ad2c641481a1193783258b2fcd64f8de5
sgrillo286 <sgrillo286@gmail.com>
2022-03-28 15:33:31 -0400

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -51,5 +51,5 @@ document.querySelectorAll('.drop').forEach(element => {
     const id = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById(id));
   });
-element.addEventListener('dragover' event => preventDefault()):
+element.addEventListener('dragover', event => preventDefault());
 });
