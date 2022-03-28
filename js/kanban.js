bf629e5f5190ed70afb45425579dc22b65f77f27
sgrillo286 <sgrillo286@gmail.com>
2022-03-28 14:50:16 -0400

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -21,7 +21,7 @@ const create_item = () => {
   item.id = 'item-' + order;
   item.draggable = true;
   item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id) );
-  item.addEventListener('dragend' event => event.dataTransfer.clearData());
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
 
   let input = doucment.createElement('input');
   item.appendChild('input');
